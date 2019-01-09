287e8ce7bd01fd30cc97598fa500701492bd2615
mcferrenm <mcferrenmax@gmail.com>
2019-01-09 17:01:28 -0500

    Return todo.id from toggleCompleted onClick

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Todo = props => {
-  return <li onClick={props.toggleCompleted}>{props.todo.task}</li>;
+  return (
+    <li id={props.todo.id} onClick={props.toggleCompleted}>
+      {props.todo.task}
+    </li>
+  );
 };
 
 export default Todo;
