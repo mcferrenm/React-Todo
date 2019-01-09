7e47d9e4a69c18e22b7eb20ded2b8c9a8f5bbfae
mcferrenm <mcferrenmax@gmail.com>
2019-01-09 18:32:55 -0500

    Add conditional style prop to li if props.todo.completed

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 const Todo = props => {
+  const lineThrough = props.todo.completed
+    ? { textDecoration: "line-through" }
+    : null;
+
   return (
-    <li id={props.todo.id} onClick={props.toggleCompleted}>
+    <li style={lineThrough} id={props.todo.id} onClick={props.toggleCompleted}>
       {props.todo.task}
     </li>
   );
